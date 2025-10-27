49248fd8eb71f332c1050109dc5e88ff5a2cab7b
Sondre Bakken <countach1992@gmail.com>
2025-10-27 15:20:21 +0100

    Modified the navbar to fit the project's requirements of having a sidebar

---
+++ group-project-2/src/components/NavbarData.tsx
@@ -0,0 +1,17 @@
+export const NavbarData = [
+  {
+    title: "Profile",
+    icon: "👤",
+    path: "/profile",
+  },
+  {
+    title: "Users",
+    icon: "👥",
+    path: "/users",
+  },
+  {
+    title: "Games",
+    icon: "🎮",
+    path: "/games",
+  },
+];
