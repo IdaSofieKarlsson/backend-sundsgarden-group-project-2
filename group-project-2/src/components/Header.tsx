43512aef372713c418dc25efc3b51643b09470bc
IdaSofieKarlsson <ida.sofie.karlsson@gmail.com>
2025-10-30 11:22:32 +0100

    starting on header, have copied all weather API code from gr proj 1

--- group-project-2/src/components/Header.tsx
+++ group-project-2/src/components/Header.tsx
@@ -1,7 +1,10 @@
+import WeatherItemMain from "../API/weatherItemMain";
+
 function Header() {
     return <div>
         <p>This is the header. It should be horisontal, unlike the vertical, to the left, navbar. <br />
         The header contains the weather API. So linking in the weather api component here. </p>
+        <WeatherItemMain/>
     </div>
 }
 
